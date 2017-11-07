e1f824b75625395944b30a0ed670161182aec956
Marco Lourenço <kriegalex@gmail.com>
2017-11-07 11:36:36 +0100

    Implemented version with separate compilation linking

---
+++ with-separate/dummy.cpp
@@ -0,0 +1,11 @@
+
+/**
+ * @license TBD
+ * @author Marco Lourenço
+ * @date 07.11.17.
+ * @copyright 2017 Intuitive Therapeutics SA. All rights reserved.
+ */
+
+/*
+ * Dummy .cpp file to avoid CLion resolve engine derping with only .cu files
+ */
